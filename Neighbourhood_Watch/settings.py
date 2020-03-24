70f25f0ac9ff7f95aa7364b0fd1eccc67f505c01
JamesOdera <nyangoma.odera@gmail.com>
2020-03-24 14:40:28 +0300

    install bootstrap and load it in the base html file for our styling

--- Neighbourhood_Watch/settings.py
+++ Neighbourhood_Watch/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'neigbourhoodapp.apps.NeigbourhoodappConfig',
+    'neigbourhoodapp',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
