580f77b8eaafe7efb0f443896c1a659a721913a9
JamesOdera <nyangoma.odera@gmail.com>
2020-03-25 11:21:32 +0300

    style the nacvbar and the image on homepage for visibility

--- Neighbourhood_Watch/settings.py
+++ Neighbourhood_Watch/settings.py
@@ -31,8 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'neigbourhoodapp',
+    'neighbourhoodapp',
     'bootstrap3',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
