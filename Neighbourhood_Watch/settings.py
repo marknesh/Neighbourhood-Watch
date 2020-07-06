57e36c800b9e4898be9f4949c80c902f7627699a
marknesh <markmunene.nesh@gmail.com>
2020-07-07 00:17:29 +0300

    email user on confirmation of account

--- Neighbourhood_Watch/settings.py
+++ Neighbourhood_Watch/settings.py
@@ -136,11 +136,11 @@ cloudinary.config(
 
 
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'markmunene.nesh@gmail.com'
-EMAIL_HOST_PASSWORD ='dvkjkgftlwifrswd'
-EMAIL_PORT = 587
+EMAIL_USE_TLS =config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
 
 
 # Password validation
